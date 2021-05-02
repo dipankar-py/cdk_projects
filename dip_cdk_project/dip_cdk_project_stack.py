d286d7d4c2540f2c9f8450b89a7e42847634cfd0
dipankar <saha.dipankar1094@gmail.com>
2021-05-02 13:04:39 +0530

    cleanup cdk.json

--- dip_cdk_project/dip_cdk_project_stack.py
+++ dip_cdk_project/dip_cdk_project_stack.py
@@ -16,4 +16,7 @@ class DipCdkProjectStack(core.Stack):
             block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
         )
 
+        mybucket=_s3.Bucket(self, "myBucketId1")
+        print(mybucket.bucket_name)
+        output_1=core.CfnOutput(self,"myBucketOutput1",value=mybucket.bucket_name,description=f"My first cdk bucket",export_name="myBucketOutput1")
         
